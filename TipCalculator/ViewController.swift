0c3c81f0c6037bbb0d7bb5da2edc4160ea84292d
Kishore Sriadibhatla <kishy104@gmail.com>
2015-04-22 12:46:00 -0700

    ios

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -12,14 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.0";
+        totalLabel.text="$0.0";
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func textAction(sender: AnyObject) {
+        
+        var tipPercentages = [0.18,0.2,0.22];
+        var percentageTip = tipPercentages[tipControl.selectedSegmentIndex];
+        
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * percentageTip;
+        
+        var total = billAmount + tip;
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
+        
+    }
+    
+    @IBAction func stepperVal(sender: AnyObject) {
+       // sender.value
+    }
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var numOFPeople: UIStepper!
+   
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
 
+    @IBOutlet weak var billField: UITextField!
 
+    @IBOutlet weak var totalLabel: UILabel!
 }
 
